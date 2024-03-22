e09ec3759e73f2c45be46b5fa7f44d7504122f1e
Sergey <s.tochenyuk@gmail.com>
2024-03-22 14:32:55 +0400

    Создаёт блок about

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,51 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+function findVideos() {
+  const videos = document.querySelectorAll('.about__video');
+
+  for (let i = 0; i < videos.length; i++) {
+    setupVideo(videos[i]);
+  }
+}
+function setupVideo(video) {
+  const link = video.querySelector('.about__video-link');
+  const media = video.querySelector('.about__video-img');
+  const button = video.querySelector('.about__video-button');
+  const id = parseMediaURL(media);
+
+  video.addEventListener('click', () => {
+    const iframe = createIframe(id);
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('about__video--enabled');
+}
+function parseMediaURL() {
+  const match = '9TZXsZItgdw?si=6dllxDfpXuFhMzsc';
+
+  return match;
+}
+
+function createIframe(id) {
+  const iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('about__video-img');
+
+  return iframe;
+}
+
+function generateURL(id) {
+  const query = '?rel=0&showinfo=0&autoplay=1';
+
+  return `https://www.youtube.com/embed/${id}${query}`;
+}
+
+findVideos();
